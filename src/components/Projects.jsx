7ce0928fd417b85a35382d80535d3afe907407b2
sidy <sidy.djimbira@gmail.com>
2025-06-25 17:20:45 +0200

    style: resize CircularGallery to 60vh and itemScale 2.5 (≈60% page)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -68,7 +68,7 @@ const Projects = () => {
         <h1 className="projects-title">PROJETS</h1>
         <p className="projects-subtitle">Découvrez mes dernières réalisations</p>
       </header>
-      <CircularGallery items={galleryItems} bend={2} borderRadius={0.09} itemScale={5} />
+      <CircularGallery items={galleryItems} bend={2} borderRadius={0.09} itemScale={2.5} />
     </div>
   );
 };
