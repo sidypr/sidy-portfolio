9bd7260cfda3c1d72eccb11b21acdb39c09d0e90
sidy <sidy.djimbira@gmail.com>
2025-06-26 19:35:37 +0200

    feat: replace background with Spline 3D animation on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import GridDistortionBackground from './components/GridDistortionBackground';
+import Spline from '@splinetool/react-spline';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <GridDistortionBackground />
+      <div className="spline-bg">
+        <Spline scene="https://prod.spline.design/6YQqQn6QvQvQvQvQ/scene.splinecode" />
+      </div>
       <div className="home-content">
         <h1 className="home-title">Sidy Djimbira</h1>
         <h2 className="home-subtitle">Développeur Web</h2>
