ac0c6492c585dd9108c202d37bb1919bdec1bd18
sidy <sidy.djimbira@gmail.com>
2025-06-26 20:39:52 +0200

    style: restore old background and apply Space Mono text-block style to home content

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import Spline from '@splinetool/react-spline';
+import GridDistortionBackground from './components/GridDistortionBackground';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="spline-bg">
-        <Spline scene="https://prod.spline.design/6YQqQn6QvQvQvQvQ/scene.splinecode" />
-      </div>
-      <div className="home-content">
+      <GridDistortionBackground />
+      <div className="text-block">
         <h1 className="home-title">Sidy Djimbira</h1>
         <h2 className="home-subtitle">Développeur Web</h2>
-        
         <div className="home-cta">
           <Link to="/projects" className="cta-button">Voir mes projets</Link>
         </div>
